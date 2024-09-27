b0f1fecce3e11a8ce5f6e493ad476b7d757343df
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-09-27 08:08:20 +0530

    array initialization

--- chapter 7/basic_array.c
+++ chapter 7/basic_array.c
@@ -2,6 +2,8 @@
 
 int main(){
     int array[2]={1,2};   // Initialize the array with two elements
+    /* it can be initialize as
+    int array[]={1,2};       without providing the size of array */
     // element in array can be input as 
     /* array[0]=1;
        array[1]=2;   */

6ff74ad0db9ab5003c2d7cd88ea4ee41bc1431d5
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-09-27 09:16:51 +0530

    passing array to the function

---
+++ chapter 7/passing_array_to_function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void modify_array(int* a,int size);
+
+int main(){
+    int array[]={55,66,85,75,45};
+    // Print original array
+    for (int i = 0; i < 5; i++)
+    {
+        printf("the element at index %d is %d\n",i,array[i]);
+    }
+    modify_array(array,5);
+    // print modified array 
+    for (int i = 0; i < 5; i++)
+    {
+        printf("the element at index %d is %d\n",i,array[i]);
+    } 
+    return 0;
+}
+void modify_array(int* a,int size){
+        for (int i = 0; i < size; i++){
+            a[i]*=2; //double the elements of array
+        }
+}

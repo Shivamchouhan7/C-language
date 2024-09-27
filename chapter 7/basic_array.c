98ca2b4048f5d3b88525a1d66c1d560191380710
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-09-27 07:54:20 +0530

    basic array program

---
+++ chapter 7/basic_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int array[2]={1,2};   // Initialize the array with two elements
+    // element in array can be input as 
+    /* array[0]=1;
+       array[1]=2;   */
+    printf("the element of index 0 is %d and of 1 is %d",array[0],array[1]); // array starts from index 0
+    return 0;
+}

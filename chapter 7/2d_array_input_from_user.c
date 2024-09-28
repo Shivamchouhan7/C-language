30104c1dfb84a8b21ce954188a3c0645e2a64ef2
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-09-28 09:00:02 +0530

    Taking input from user in 2d array

--- chapter 7/2d_array_input_from_user.c
+++ chapter 7/2d_array_input_from_user.c
@@ -3,15 +3,13 @@
 int main() {
     // Initialize a 3x2 array
     int array[3][2];
-    int row,column,e;
+    int row,column;
     
     // Loop through each element of the array
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
-            printf("enter element of  row %d and column  %d :",i,j);
-            scanf("%d",&e);
-            array[i][j]=e;
-            
+            printf("enter value of  array[%d][%d]:",i,j);
+            scanf("%d",&array[i][j]);
         }
         
     }

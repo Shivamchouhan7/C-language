5edbb7e74db8a50cef232ed78335bf06a71d04aa
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-10-02 13:54:49 +0530

    printing string using format specifier

---
+++ chapter 8/printing_strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//PRINTING STRINGS WITHOUT USING LOOP
+int main(){
+    // Initializing a string using double quotes
+    char string[]="Hello";
+    /* for (int i = 0; i <3; i++)
+       {
+       printf(" character is %c\n",string1[i]); 
+       } */
+    // Directly printing the string using the format specifier for strings
+    printf("%s \n", string); 
+    return 0;
+}

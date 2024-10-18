9bcff9468a8d555d16a4f40e9a195b82c73e1d49
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-10-18 22:58:35 +0530

    pointer to structure

---
+++ chapter 9/pointer_to_structure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Define the structure
+struct data {
+    int amount;
+};
+
+int main() {
+    struct data a1;          // Create an instance of the structure
+    a1.amount = 50000; 
+
+    struct data *ptr;       // Declare a pointer to the structure
+    ptr = &a1;              // Assign the address of a1 to the pointer
+
+    // Accessing the amount using pointer dereferencing
+    printf("The amount is %d \n", (*ptr).amount); // Using dereferencing
+    printf("The amount is %d \n", ptr->amount);   // Using the arrow operator
+
+    return 0;
+}

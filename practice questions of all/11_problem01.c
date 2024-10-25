7f45b6afc9e758035cec5ae28d576902fbaadca8
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-10-25 14:14:03 +0530

    dynamically create an array of size 6  storing 6 integers

---
+++ practice questions of all/11_problem01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+// program to dynamically create an array of size 6 capable of storing 6 integers
+int main(){
+    int n=6,num;
+    int *ptr;
+    ptr=(int*)malloc(n*sizeof(int));
+    for(int i=0;i<6;i++){
+        printf("Enter element %d : ",i+1);
+        scanf("%d",&ptr[i]);
+    }
+    for(int i=0;i<6;i++){
+        printf("Element %d : %d\n ",i+1,ptr[i]);
+    }
+
+    return 0;
+}
